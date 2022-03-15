15d0d5937e7cc61728bfea25dd556716cdea1030
Joanna <m.joanna.foss@gmail.com>
2022-03-15 11:35:09 -0500

    search results section

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+//EVENT LISTENERS
+//Window Load
 window.addEventListener("load", () => {
     setTimeout(() => {
         let loader = document.getElementById("load-container")
         loader.remove()
     }, 1500)    
 })
+//Search bar
+document.getElementById("search-button").addEventListener("click", () => {
+    let userSearch = document.getElementById("search-bar").value
+    query(userSearch, 1)
+})
+
+//Quick links
+let quickLink = document.getElementsByClassName("quick-link")
+for(let i = 0; i < quickLink.length; i++) {
+    quickLink[i].addEventListener("click", () => {
+        query(quickLink[i].innerHTML, 1);
+    })
+}
 
 const searchspringURL = "https://api.searchspring.io/api/search/search.json"
 
+
+
+
+//Function definitions
 function query(term, pageNumber) {
+    if(document.getElementById("search-results").classList.contains("d-none")) {
+        document.getElementById("search-results").classList.remove("d-none")
+    }
     const searchParameters = new URLSearchParams({
         q: term,
         resultsFormat: "native",
@@ -19,9 +41,22 @@ function query(term, pageNumber) {
         fetch(searchspringURL + "?" + searchParameters)
             .then(response => response.json())
             .then(data => {
-                resolve(data)
+                // resolve(data)
+                let pageInfo = createPageDataObject(data)
+                document.getElementById("user-searched").innerText = term
                 console.log(data)
             })
             .catch(error => console.log(error))
     })
+}
+
+function createPageDataObject(searchData) {
+    let pageObject = {
+        current: searchData.pagination.currentPage,
+        next: searchData.pagination.nextPage,
+        previous: searchData.pagination.previousPage
+    }
+
+    console.log(pageObject)
+    return pageObject;
 }
